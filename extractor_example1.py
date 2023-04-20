9eba4000ba9172fff5fc85df37fc0cf84e5086f7
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-20 14:10:02 +0200

    Update extractor_example1.py

--- extractor_example1.py
+++ extractor_example1.py
@@ -1,28 +1,38 @@
+# Import necessary modules
+import argparse
+import logging
 
 
 
 
-
-
-
-
-# Example arguments 
+# Set up logger
 logger = logging.getLogger(__name__)
-lh = logging.StreamHandler()  # Handler for the logger
+lh = logging.StreamHandler()
 logger.addHandler(lh)
 formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt='%H:%M:%S')
 lh.setFormatter(formatter)
-# 5.2 parse your arguments
+
+# Set up argument parser
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-xml', action='generate_xml', dest='export_xml',
+# Add argument for exporting data as XML
+parser.add_argument('-xml', action='store_true', dest='export_xml',
                     help='Export data in a .xml-file')
-parser.add_argument('-csv', action='generate_csv', dest='export_csv',
+
+# Add argument for exporting data as CSV
+parser.add_argument('-csv', action='store_true', dest='export_csv',
                     help='Export data in a .csv-file')
-parser.add_argument('-txt', action='generate_txt', dest='export_txt',
+
+# Add argument for exporting data as TXT
+parser.add_argument('-txt', action='store_true', dest='export_txt',
                     help='Export data in a .txt-file')
-parser.add_argument('-sqlite', action='generate_sqlite', dest='export_sqlite',
-                    help='Space separated list of cookies',
-                    nargs='+', default=[])
+
+# Add argument for exporting data to SQLite database
+parser.add_argument('-sqlite', action='store_true', dest='export_sqlite',
+                    help='Export data to a SQLite database')
+
+# Parse arguments
 results = parser.parse_args()
+
+# Set logger level based on verbosity flag
 logger.setLevel(logging.DEBUG if results.verbose else logging.INFO)
